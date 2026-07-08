e84b5a88ae0f41a411c82616450080d5b5d867a8
tot437 <sosomakerone@gmail.com>
2026-07-08 16:31:03 +0300

    fineshed 3 products styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { HomePage } from './pages/HomePage';
+import './pages/homepages/header.css';
+import './pages/homepage/homepage.css';
+import './App.css'
+
+function App() {
+  return (
+    <HomePage/>
+  );
+}
+
+export default App
