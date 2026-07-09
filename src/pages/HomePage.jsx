69ea990d5315fe3bc3a8a4d9339a3a774d3da94b
tot437 <sosomakerone@gmail.com>
2026-07-09 17:07:32 +0300

    generate html for the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 
 import './homepages.css';
 import Header from '../commponents/header';
+import Products from '../../products';
 
 
 export default function HomePage() {
@@ -13,17 +14,19 @@ export default function HomePage() {
 
         <div className="home-page">
           <div className="products-grid">
-
-            <div className="product-container">
+             {Products.map((product)=>{
+              return(
+                <>
+                    <div key={product.id} className="product-container">
               <div className="product-image-container">
-                <img className="product-image" src="./public/images/products/athletic-cotton-socks-6-pairs.jpg" alt="Black and Gray Athletic Cotton Socks - 6 Pairs" />
+                <img className="product-image" src={product.image} alt={product.name} />
               </div>
-              <div className="product-name limit-text-to-2-lines">Black and Gray Athletic Cotton Socks - 6 Pairs</div>
+              <div className="product-name limit-text-to-2-lines">{product.name}</div>
               <div className="product-rating-container">
                 <img className="product-rating-stars" src="./public/images/ratings/rating-45.png" alt="4.5 star rating" />
                   <div className="product-rating-count link-primary">87</div>
               </div>
-              <div className="product-price">$10.90</div>
+              <div className="product-price">$ ${ (product.priceCents / 100).toFixed(2) }</div>
               <div className="product-quantity-container">
                 <select>
                   <option value="1">1</option><option value="2">2</option><option value="3">3</option>
@@ -39,58 +42,12 @@ export default function HomePage() {
               </div>
               <button className="add-to-cart-button button-primary">Add to Cart</button>
             </div>
+                </>
+              );
+             })}
+           
 
-            <div className="product-container">
-              <div className="product-image-container">
-                <img className="product-image" src="./public/images/products/intermediate-composite-basketball.jpg" alt="Intermediate Size Basketball" />
-              </div>
-              <div className="product-name limit-text-to-2-lines">Intermediate Size Basketball</div>
-              <div className="product-rating-container">
-                <img className="product-rating-stars" src="./public/images/ratings/rating-40.png" alt="4.0 star rating" />
-                  <div className="product-rating-count link-primary">127</div>
-              </div>
-              <div className="product-price">$20.95</div>
-              <div className="product-quantity-container">
-                <select>
-                  <option value="1">1</option><option value="2">2</option><option value="3">3</option>
-                  <option value="4">4</option><option value="5">5</option><option value="6">6</option>
-                  <option value="7">7</option><option value="8">8</option><option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-              <div className="product-spacer"></div>
-              <div className="added-to-cart">
-                <img src="./public/images/icons/checkmark.png" alt="Checkmark" />
-                  Added
-              </div>
-              <button className="add-to-cart-button button-primary">Add to Cart</button>
-            </div>
-
-            <div className="product-container">
-              <div className="product-image-container">
-                <img className="product-image" src="./public/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" alt="Adults Plain Cotton T-Shirt - 2 Pack" />
-              </div>
-              <div className="product-name limit-text-to-2-lines">Adults Plain Cotton T-Shirt - 2 Pack</div>
-              <div className="product-rating-container">
-                <img className="product-rating-stars" src="./public/images/ratings/rating-45.png" alt="4.5 star rating" />
-                  <div className="product-rating-count link-primary">56</div>
-              </div>
-              <div className="product-price">$7.99</div>
-              <div className="product-quantity-container">
-                <select>
-                  <option value="1">1</option><option value="2">2</option><option value="3">3</option>
-                  <option value="4">4</option><option value="5">5</option><option value="6">6</option>
-                  <option value="7">7</option><option value="8">8</option><option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-              <div className="product-spacer"></div>
-              <div className="added-to-cart">
-                <img src="./public/images/icons/checkmark.png" alt="Checkmark" />
-                  Added
-              </div>
-              <button className="add-to-cart-button button-primary">Add to Cart</button>
-            </div>
+            
           </div>
         </div>
       </>
