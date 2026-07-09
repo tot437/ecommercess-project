18cac125d668f436f85281fe8039b412084c3847
tot437 <sosomakerone@gmail.com>
2026-07-09 17:56:54 +0300

    fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,11 @@ import Products from '../../products';
 
 
 export default function HomePage() {
+  fetch('http://localhost:3000/api/products').then((response)=>{
+    response.json().then((data)=>{
+      data.json();
+    });
+  })
   return (
     <>
       
