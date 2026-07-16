61e11882735e89d1d3ab6050452dad81c6db0863
tot437 <sosomakerone@gmail.com>
2026-07-16 16:25:40 +0300

    create integration test for the product component

---
+++ src/pages/home/productes.test.jsx
@@ -0,0 +1,23 @@
+import { it, expect, describe } from "vitest";
+// eslint-disable-next-line no-unused-vars
+import productes from './Product ';
+import { render } from "@testing-library/react";
+
+describe('product commponent',()=>{
+  it('desplay product details correctly',()=>{
+    // eslint-disable-next-line no-unused-vars
+    const product = {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+            stars: 4.5,
+            count: 87
+        }
+    }
+    // eslint-disable-next-line react/no-unknown-property, no-undef
+    render(<productes  Product={product} />)
+    expect()
+  });
+});
+
